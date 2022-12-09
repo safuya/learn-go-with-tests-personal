ba228f598aa2295cad87fc2ed7c64e5d663a8dfe
Sophie Hughes <sophie.hughes@mettle.co.uk>
2022-12-09 11:21:25 +0000

    Add wallet for pointers lesson

---
+++ pointers-and-errors/wallet_test.go
@@ -0,0 +1,18 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if want != got {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
