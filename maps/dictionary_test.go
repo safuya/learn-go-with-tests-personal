573b5fef982849cbc690a6b1cc639131259a0e23
Sophie Hughes <sophie.hughes@mettle.co.uk>
2022-12-12 07:05:52 +0000

    Initial dictionary implementation

---
+++ maps/dictionary_test.go
@@ -0,0 +1,20 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	AssertString(t, got, want)
+}
+
+func AssertString(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
