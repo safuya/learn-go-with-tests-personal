c5494398df145f482bbffe64041b9bfc9bd0c423
Sophie Hughes <sophie.hughes@mettle.co.uk>
2022-12-12 07:30:30 +0000

    Use custom error for word not found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,17 @@
 package dictionary
 
+import "errors"
+
+var ErrNotFound = errors.New("the word you are looking for can't be found")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,20 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		AssertString(t, got, want)
+	})
 
-	AssertString(t, got, want)
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected to get an error")
+		}
+		AssertError(t, got, ErrNotFound)
+	})
 }
 
 func AssertString(t testing.TB, got, want string) {
@@ -16,5 +26,13 @@ func AssertString(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func AssertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q wanted error %q", got, want)
 	}
 }
