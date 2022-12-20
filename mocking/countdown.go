7b5891b05311f91241cf389b0a816966508cf88b
Sophie Hughes <sophie.hughes@mettle.co.uk>
2022-12-20 03:28:12 +0000

    Add mocking chapter
    Must commit more often.

---
+++ mocking/countdown.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+
+	for counter := countdownStart; counter > 0; counter-- {
+		fmt.Fprintln(out, counter)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+const sleep = "sleep"
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+const write = "write"
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run("test the output", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("Got %q but wanted %q", got, want)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("Got %d calls to sleeper, wanted 3", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep after every countdown", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
