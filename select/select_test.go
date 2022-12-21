553c535ecd895936bd69a4b0f33cf334f3510e9a
Sophie Hughes <sophie.hughes@mettle.co.uk>
2022-12-21 03:11:24 +0000

    Refactor server creation for TestRacer

--- select/select_test.go
+++ select/select_test.go
@@ -8,14 +8,10 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	defer slowServer.Close()
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+	defer fastServer.Close()
 
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
@@ -26,7 +22,11 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("Got %q, wanted %q", got, want)
 	}
+}
 
-	slowServer.Close()
-	fastServer.Close()
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
